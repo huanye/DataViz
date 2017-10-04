15061ee72ad0e225e401756a74ded17746e9e8dd
Huanye Liu <huanyeliu@Huanyes-MacBook-Pro.local>
2017-10-04 11:07:40 -0500

    load data into R

--- IntroToR/IntroToR.R
+++ IntroToR/IntroToR.R
@@ -1,6 +1,14 @@
+# load libraries
 library(readr)
 library(haven)
 library(dplyr)
 library(tidyr)
 library(stringr)
 library(ggplot2)
+
+# load data into R
+acc2014 <- read_sas("accident.sas7bdat")
+acc2015 <- read_csv("accident.csv")
+ls()
+class(acc2014)
+class(acc2015)
